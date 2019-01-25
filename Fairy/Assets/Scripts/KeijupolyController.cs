05a070f7231e054937f0a6e129f8beb27305e295
Eeva Maria Tolonen <eeva.m.tolonen@student.jyu.fi>
2019-01-25 14:15:06 +0200

    korjattu viivanpiirtoscripti

---
+++ Fairy/Assets/Scripts/KeijupolyController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeijupolyController : MonoBehaviour
+{// miksi float?
+    //private float buginKorjausTaikanumero = 0;
+
+    [SerializeField]
+    private LineRenderer lineRenderer;
+    [SerializeField]
+    private EdgeCollider2D edgeCollider;
+    [SerializeField]
+    private Camera mainCamera;
+    private List<Vector2> mousePoints;
+    private Vector2[] colliderPoints;
+
+    private float polunMaksimi = 100; // lisäämäni muuttuja
+
+
+    void Awake()
+    {
+        CreateLineRenderer();
+        CreateEdgeCollider();
+
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+
+        mousePoints = new List<Vector2>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Reset();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+            if (!mousePoints.Contains(mousePosition) && mousePoints.Count < polunMaksimi) // tätä riviä muutettu
+            {
+                mousePoints.Add(mousePosition);
+                lineRenderer.positionCount = mousePoints.Count;
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, mousePosition);
+            }
+
+            //mousePoints.GetRange(0, 100);
+
+            // float 
+            colliderPoints = mousePoints.ToArray();
+
+             /*for (int i = 0; i < colliderPoints.Length; i++)
+             {
+                 colliderPoints[i].x += buginKorjausTaikanumero;
+             }*/
+             edgeCollider.points = colliderPoints;
+        }
+    }
+
+    private void Reset()
+    {
+        /* ei ymmärtääkseni tarvita, silloin viiva jää myös olemaan maailmaan ja häviää vasta seuraavan viivan luomiseen
+         if (lineRenderer != null)
+         {
+             lineRenderer.positionCount = 0;
+         }*/
+        if (mousePoints != null)
+        {
+            mousePoints.Clear();
+        }
+        if (edgeCollider != null)
+        {
+            edgeCollider.Reset();
+        }
+    }
+
+    private void CreateEdgeCollider()
+    {
+        edgeCollider = new GameObject("collider").AddComponent<EdgeCollider2D>();
+    }
+
+    private void CreateLineRenderer()
+    {
+        lineRenderer = new GameObject("renderer").AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 0;
+        //lineRenderer.material = new Material(Shader.Find("Particles/Standard Surface"));
+        lineRenderer.startColor = Color.black;
+        lineRenderer.endColor = Color.black;
+        lineRenderer.startWidth = 0.2f;
+        lineRenderer.endWidth = 0.2f;
+        lineRenderer.useWorldSpace = true;
+    }
+}
---
+++ Fairy/Assets/Scripts/PelaajaController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelaajaController : MonoBehaviour
+{
+
+    public float maxSpeed = 10f;
+    private Rigidbody2D pallo;
+    private float movement;
+    private float jumpForce = 350f;
+
+    public bool grounded = false;
+    float groundRadius = 0.2f;
+    // tarkistaa, onko pallon alaosaan liitetty gameObject groundCheck maassa vai ei
+    public LayerMask whatIsGround;
+    // Every object in a Scene has a Transform. It's used to store and manipulate the position, rotation and scale of the object.
+    public Transform groundCheck; // t.ex. groundCheck.position, groundCheck.scale etc.
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // eli overläppääkö pallon alaosaan liitetty gameObject groundCheck maaksi määritellyn alueen kanssa 
+        //                         centre of the circle, radius of circle, filter to check objects only on spesific layers (kenttalayer, ei pelaajaLayer)
+        // Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        pallo = GetComponent<Rigidbody2D>();
+        movement = Input.GetAxis("Horizontal");
+        pallo.velocity = new Vector2(movement * maxSpeed, pallo.velocity.y);
+        //pallo.transform.localScale = groundCheck.localScale;
+
+        // odotetaan välilyönnin painallusta ja hypätään
+        if(Input.GetKeyDown(KeyCode.W) && grounded) // tee oma Jump-button, jonka pelaaja voi remapata haluamakseen, tee siis oikeaan peliin paremmin!
+        {
+            grounded = false;
+            pallo.AddForce(new Vector2(0, jumpForce));
+        }
+
+        // seuraavaksi hyppy == ts. vektori ylöspäin (AddForce() hmmm, toisaalta gravity? Jep, AddForce(0, 700)
+    }
+}
