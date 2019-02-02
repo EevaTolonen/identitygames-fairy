d5f826e49600f4726082554cf6128db1f40b411e
Eeva Maria Tolonen <eeva.m.tolonen@student.jyu.fi>
2019-02-02 17:32:02 +0200

    Korjattu peliluolassa esille tulleet ongelmat: parannettu hyppy ja pelaajaa seuraava kamera korvattu kahdella kameralla

--- Fairy/Assets/Scripts/PelaajaController.cs
+++ Fairy/Assets/Scripts/PelaajaController.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PelaajaController : MonoBehaviour
 {
-
-    public float nopeusKerroinIlmassa = 2;
+    public Cinemachine.CinemachineVirtualCamera CameraLeft2Right;
+    public Cinemachine.CinemachineVirtualCamera CameraRight2Left;
+    public float putoamisNopeus;
+    public float nopeusIlmassaJaettuna = 1.5F;
     public float maxSpeed = 10f;
     private Rigidbody2D pelaaja;
     private float movement;
     public float jumpForce = 350f;
+    private bool facingRight = true;
 
     public bool grounded = false;
     float groundRadius = 0.2f;
@@ -22,25 +26,82 @@ public class PelaajaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // eli overläppääkö pallon alaosaan liitetty gameObject groundCheck maaksi määritellyn alueen kanssa 
-        //                         centre of the circle, radius of circle, filter to check objects only on spesific layers (kenttalayer, ei pelaajaLayer)
-        // Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        OllaankoMaassa();
+
+        HaePelaajaJaAxis();
+
+        MitenSaaLiikkua();
+
+        SaakoHypata();
+
+        KameraVasenOikea();
+    }
+
+
+
+    // ts. mahd. skenaariot: pelaaja menee oikealle/paikallaan ja kamera ko. hetkellä l2r -> pysyy samana
+    // pelaaja menee oikealle/paikallaan ja kamera r2l -> vaihtuu
+    // pelaaja menee vasemmalle/paikallaan ja kamera ko. hetkellä r2l -> pysyy samana
+    // pelaaja menee vasemmalle/paikallaan ja kamera l2r -> vaihtuu
+
+    private void KameraVasenOikea()
+    {
+        if (movement * maxSpeed < 0 && facingRight )
+        {
+            CameraLeft2Right.enabled = false;
+            CameraRight2Left.enabled = true;
+            facingRight = !facingRight;
+
+        }
+        // if (pelaaja.velocity.x > 0 && CameraLeft2Right.enabled == true
+        if (movement * maxSpeed > 0 && !facingRight)
+        {
+            CameraRight2Left.enabled = false;
+            CameraLeft2Right.enabled = true;
+            facingRight = !facingRight;
+        }
+    }
+
+
+
+    private void HaePelaajaJaAxis()
+    {
         pelaaja = GetComponent<Rigidbody2D>();
         movement = Input.GetAxis("Horizontal");
-        // 
-        if(grounded) pelaaja.velocity = new Vector2(movement * maxSpeed, pelaaja.velocity.y);
-        // pelaajan nopeus puolitetaan, kun ilmassa, ettei tule naurettavia hyppyjä
-        if(!grounded) pelaaja.velocity = new Vector2(movement * maxSpeed / nopeusKerroinIlmassa, pelaaja.velocity.y);
-        //pallo.transform.localScale = groundCheck.localScale;
+    }
 
+
+
+    // eli overläppääkö pallon alaosaan liitetty gameObject groundCheck maaksi määritellyn alueen kanssa 
+    // parametrit: centre of the circle, radius of circle, filter to check objects only on spesific layers (kenttalayer, ei pelaajaLayer)
+    // Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+    private void OllaankoMaassa()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+    }
+
+
+
+    void MitenSaaLiikkua()
+    {
+        if (grounded) pelaaja.velocity = new Vector2(movement * maxSpeed, pelaaja.velocity.y);
+        // pelaajan nopeutta hidastetaan, kun ilmassa, ettei tule naurettavia hyppyjä
+        if (!grounded && pelaaja.velocity.y <= 0)
+        {   // pelaaja putoaa alas hieman nopeammin kuin hyppäsi, jotta ei tule outoa "leijumisefektiä" alas pudotessa
+            pelaaja.AddForce(new Vector2(0, putoamisNopeus));
+        }
+        else pelaaja.velocity = new Vector2(movement * maxSpeed / nopeusIlmassaJaettuna, pelaaja.velocity.y);
+    }
+
+
+
+    void SaakoHypata()
+    {
         // odotetaan välilyönnin painallusta ja hypätään
         if (Input.GetKeyDown(KeyCode.W) && grounded) // tee oma Jump-button, jonka pelaaja voi remapata haluamakseen, tee siis oikeaan peliin paremmin!
         {
             grounded = false;
             pelaaja.AddForce(new Vector2(0, jumpForce));
         }
-
-        // seuraavaksi hyppy == ts. vektori ylöspäin (AddForce() hmmm, toisaalta gravity? Jep, AddForce(0, 700)
     }
 }
