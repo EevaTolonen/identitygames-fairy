48f7619be4f4977b9f13abbef676a9eafb105083
ollitp <olli.paakkunainen@gmail.com>
2019-02-15 12:08:07 +0200

    Created dialog system

---
+++ Fairy/Assets/Scripts/DialogSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogSystem : MonoBehaviour
+{
+    /// <summary>
+    /// Shows given text at dialog window.
+    /// </summary>
+    public void ShowText(string txt) {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Clears dialog window.
+    /// </summary>
+    public void Clear() {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Shows given dialog choices in dialog window and returns index of player made choice
+    /// </summary>
+    public int GetChoiceIndex(string[] txtArr) {
+        throw new NotImplementedException();
+    }
+}
