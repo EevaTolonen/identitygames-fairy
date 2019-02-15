2edf9b5f9db9e3919c715d70c1b64907bd74d295
ollitp <olli.paakkunainen@gmail.com>
2019-02-16 01:04:26 +0200

    Added dialog system placeholder containers

--- Fairy/Assets/Scripts/DialogSystem.cs
+++ Fairy/Assets/Scripts/DialogSystem.cs
@@ -2,21 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogSystem : MonoBehaviour
 {
+
+    private TextMeshProUGUI dialogContainer;
+    private TextMeshProUGUI[] dialogChoiceContainers;
+
     /// <summary>
     /// Shows given text at dialog window.
     /// </summary>
-    public void ShowText(string txt) {
-        throw new NotImplementedException();
+    public void SetText(string txt) {
+        dialogContainer.text = txt;
     }
 
     /// <summary>
     /// Clears dialog window.
     /// </summary>
     public void Clear() {
-        throw new NotImplementedException();
+        dialogContainer.text = "";
     }
 
     /// <summary>
