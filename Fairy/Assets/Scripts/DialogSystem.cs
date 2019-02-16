5cd2c05a2230e30cf44393ac492742d53ad799c6
ollitp <olli.paakkunainen@gmail.com>
2019-02-16 14:53:09 +0200

    Dialog class created

--- Fairy/Assets/Scripts/DialogSystem.cs
+++ Fairy/Assets/Scripts/DialogSystem.cs
@@ -2,30 +2,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
+/// <summary>
+/// Handles dialog system implemented by canvas. 
+/// Methods are intended to be called by triggers on game level.
+/// </summary>
 public class DialogSystem : MonoBehaviour
 {
+    /// <summary>
+    /// Images are merely used for sizing and positioning of textboxes
+    /// and can be disabled after wake if needed.
+    /// </summary>
+    [Header("Containers:")]
+    public Image dialogContainer;
+    public Image choiceContainer;
 
-    private TextMeshProUGUI dialogContainer;
-    private TextMeshProUGUI[] dialogChoiceContainers;
+    private TextMeshProUGUI dialogTextbox;
+    private TextMeshProUGUI[] choiceTextboxArray;
 
     /// <summary>
     /// Shows given text at dialog window.
     /// </summary>
     public void SetText(string txt) {
-        dialogContainer.text = txt;
+        dialogTextbox.text = txt;
     }
 
     /// <summary>
     /// Clears dialog window.
     /// </summary>
     public void Clear() {
-        dialogContainer.text = "";
+        dialogTextbox.text = "";
     }
 
     /// <summary>
-    /// Shows given dialog choices in dialog window and returns index of player made choice
+    /// Shows given dialog choices in dialog window 
+    /// and returns index of player made choice.
     /// </summary>
     public int GetChoiceIndex(string[] txtArr) {
         throw new NotImplementedException();
