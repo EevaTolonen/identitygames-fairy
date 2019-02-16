b3b5306023bca63a1e7fe7dcc7cc6bc765310549
ollitp <olli.paakkunainen@gmail.com>
2019-02-17 01:47:34 +0200

    Restructured dialog system

--- Fairy/Assets/Scripts/Dialog.cs
+++ Fairy/Assets/Scripts/Dialog.cs
@@ -3,54 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Composing a complete dialog out of DialogScreens.
+/// </summary>
 public class Dialog
 {
-    public struct DialogText
-    {
-        public int id;
-        public string text;
+    
 
-        public DialogText(int i, string t)
-        {
-            id = i;
-            text = t;
-        }
-    }
-
-    public struct Choice
-    {
-        int nextDialogId;
-        string text;
-
-        public Choice(int i, string t)
-        {
-            nextDialogId = i;
-            text = t;
-        }
-    }
-
-    private List<DialogText> dialogTexts = new List<DialogText>();
-    private List<Choice> choices = new List<Choice>();
-
-    public void AddDialogText(int id, string txt)
-    {
-        DialogText dialog = new DialogText(id, txt);
-        dialogTexts.Add(dialog);
-    }
-
-    public void AddChoice(int idToNextDialog, string txt)
-    {
-        Choice choice = new Choice(idToNextDialog, txt);
-        choices.Add(choice);
-    }
-
-    public DialogText GetDialogTextById(int id)
-    {
-        foreach(DialogText dText in dialogTexts)
-        {
-            if (dText.id == id) return dText;
-        }
-
-        throw new ArgumentException("Dialog text with given id not found");
-    }
 }
---
+++ Fairy/Assets/Scripts/DialogScreen.cs
@@ -0,0 +1,43 @@
+﻿//@author Olli Paakkunainen
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Part of whole dialog. Contains all infomation what is shown at once.
+/// A dialog text and player responses to it.
+/// </summary>
+public class DialogScreen
+{
+    //text shown
+    private string text;
+
+    //identifier
+    private int id;
+
+    //int -> identifier of next DialogScreen | string -> text shown
+    private Dictionary<int, string> responses;
+
+    public DialogScreen(int id, string text, Dictionary<int, string> choices)
+    {
+        this.id = id;
+        this.text = text;
+        this.responses = choices;
+    }
+
+    public DialogScreen(int id, string text)
+    {
+        this.id = id;
+        this.text = text;
+        responses = null;
+    }
+
+    public DialogScreen(int id)
+    {
+        this.id = id;
+        this.text = "";
+        responses = null;
+    }
+}
