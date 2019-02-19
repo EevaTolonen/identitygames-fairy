7e48e389bc925c025392fd2462c42cc11fe04d65
ollitp <olli.paakkunainen@gmail.com>
2019-02-19 15:44:11 +0200

    Dialog changes

--- Fairy/Assets/Scripts/Dialog.cs
+++ Fairy/Assets/Scripts/Dialog.cs
@@ -8,6 +8,24 @@ using UnityEngine;
 /// </summary>
 public class Dialog
 {
-    
+    public enum DialogState { Idle, WaitForResponse }
+
+    private DialogState state;
+
+    /// <summary>
+    /// Clears dialog text and response options and sets state to idle
+    /// </summary>
+    public void ReturnToIdle()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Shows given dialog text and response options related to it and sets state to wait
+    /// </summary>
+    public void ShowDialogScreen(DialogScreen dialogScreen)
+    {
+        throw new NotImplementedException();
+    }
 
 }
