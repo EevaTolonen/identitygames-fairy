15936d5db589c37399badfd9412be2c7f2d3e8b6
Eeva Maria Tolonen <eeva.m.tolonen@student.jyu.fi>
2019-03-19 22:02:43 +0200

    Made minor changes to vaihdokki movement so that vaihdokki is always positioned behind the player

---
+++ Fairy/Assets/FollowCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 vali;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vali = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + vali;
+    }
+}
--- Fairy/Assets/Scripts/VaihdokkiMovement.cs
+++ Fairy/Assets/Scripts/VaihdokkiMovement.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityStandardAssets._2D;
 using UnityEngine;
+
+
 
 public class VaihdokkiMovement : MonoBehaviour
 {
@@ -9,15 +12,24 @@ public class VaihdokkiMovement : MonoBehaviour
     public float baseSpeed = 10f;
     public float maxDistance = 2;
     public float maxDistanceSpeedModifier = 2;
+    public bool facingRight = true;
+
 
     private void Awake()
     {
+
         if (followTarget == null)
             followTarget = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Update()
     {
+
+        // we see whether player is facing right or not, and position changeling accordingly behind the player
+        facingRight = followTarget.GetComponent<PlatformerCharacter2D>().GetPlayerFacingRight();
+        if (!facingRight && offset.x < 0) offset.x *= -1;
+        if (facingRight && offset.x > 0) offset.x *= -1;
+
         float moveSpeed = baseSpeed;
         float distanceToTarget = Vector3.Distance(transform.position, followTarget.transform.position + offset);
         Debug.Log(distanceToTarget);
